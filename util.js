c1ab6547d8e9ec1f5104a4463ac6f26e1f2793b9
javion25 <javion25@foxmail.com>
2018-07-11 17:03:33 +0800

    Update util.js

--- util.js
+++ util.js
@@ -1,4 +1,6 @@
-// 环境判断
+/**
+ * 判断执行环境
+ */
 export const UA = inBrowser && window.navigator.userAgent.toLowerCase()
 export const isIE = UA && /msie|trident/.test(UA)
 export const isIE9 = UA && UA.indexOf('msie 9.0') > 0
@@ -7,13 +9,17 @@ export const isAndroid = (UA && UA.indexOf('android') > 0) || (weexPlatform === 
 export const isIOS = (UA && /iphone|ipad|ipod|ios/.test(UA)) || (weexPlatform === 'ios')
 export const isChrome = UA && /chrome\/\d+/.test(UA) && !isEdge
 
-// 获取Url参数
-export const getQueryString = (name) => {
-  let reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
-  let r = window.location.search.substr(1).match(reg);
-  if (r != null) {
-    return unescape(r[2]);
-  }
-  return null;
+/**
+ * 获取Url参数
+ * name: 参数名， url: 可选（非浏览器环境必选）
+ */
+export const getQueryString = (name, url) => {
+	let target = url ? url.substr(url.indexOf('?')) : window.location.search
+	let reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i')
+	let r = target.substr(1).match(reg)
+	if (r !== null) {
+		return decodeURI(r[2])
+	}
+	return null
 }
 
