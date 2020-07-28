0288d73739ec08bc2890fe208eceb81ce858c794
周佳维 <zhoujiawei@qutoutiao.net>
2020-07-28 20:33:55 +0800

    feat: update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * 判断执行环境
  */
-export const UA = inBrowser && window.navigator.userAgent.toLowerCase()
+export const UA = window.navigator.userAgent.toLowerCase()
 export const isIE = UA && /msie|trident/.test(UA)
 export const isIE9 = UA && UA.indexOf('msie 9.0') > 0
 export const isEdge = UA && UA.indexOf('edge/') > 0
